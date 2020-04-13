a5754010b42ba06f7861bfb998afb5ada1238df1
Daniel <dcca12@gmail.com>
2020-04-13 09:28:56 -0300

    finish the first module

--- app.js
+++ app.js
@@ -1,39 +1,10 @@
+// Core modules
 const http = require('http');
-const fs = require('fs');
+// Custom Files
+const routes = require('./route');
 
+const server = http.createServer(routes.handler);
 
-const server = http.createServer((req, res) => {
-    const url = req.url;
-    const method = req.method;
-    if (url === '/') {
-	res.setHeader('Content-Type', 'text/html');
-	res.write('<html>')
-	res.write('<head><title>My test</title></head>')
-	res.write('<body><form action="/message" method="POST"><input type="text" name ="message"></input></form></body>')
-	res.write('</html>')
-	return res.end();
-    }
-    if(url === '/message' && method === 'POST'){
-	const body = [];
-	req.on('data', (chunk) => {
-	    console.log(chunk);
-	    body.push(chunk);
-	});
-	return req.on('end', () => {
-	    const parseBody = Buffer.concat(body).toString();
-	    const message = parseBody.split('=')[1];
-	    fs.writeFile('message.txt', message, err => {
-		res.writeHead(302, {'Location': '/'})
-		return res.end();
-	    });
-	});
-    }
-    res.setHeader('Content-Type', 'text/html');
-    res.write('<html>')
-    res.write('<head><title>My test</title></head>')
-    res.write('<body><h1>Hi there!</h1></body>')
-    res.write('</html>')
-    res.end();
-});
+console.log('Start server');
 
 server.listen(8000);
