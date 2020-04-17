8975791ab0a7523951a23210e9fc6a5285523730
Daniel <dcca12@gmail.com>
2020-04-17 09:41:16 -0300

    .gitignore fix

--- module7/util/database.js
+++
@@ -1,8 +0,0 @@
-const Sequelize = require('sequelize');
-
-const sequelize = new Sequelize('node-complete', 'wbench', 'D@ni12345', {
-    dialect: 'mysql', 
-    host: 'localhost'
-});
-
-module.exports = sequelize;
