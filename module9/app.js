d4036c30ab302c75f426e04ae2d743cf641a1ca1
dcca <dcca12@gmail.com>
2020-04-27 18:54:35 -0300

    finish mongoose module

---
+++ module9/app.js
@@ -0,0 +1,55 @@
+const path = require('path');
+
+const express = require('express');
+const bodyParser = require('body-parser');
+const dbConnection = require('./util/database');
+
+const errorController = require('./controllers/error');
+const User = require('./models/user');
+
+const app = express();
+
+app.set('view engine', 'ejs');
+app.set('views', 'views');
+
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use((req, res, next) => {
+	User.findById('5ea7453b923fe8a922fc649a')
+		.then((user) => {
+			req.user = user;
+			next();
+		})
+		.catch((err) => console.log(err));
+});
+
+app.use('/admin', adminRoutes);
+app.use(shopRoutes);
+
+app.use(errorController.get404);
+
+// Database connection
+dbConnection
+	.then((result) => {
+		User.findOne().then((user) => {
+			if (!user) {
+				const user = new User({
+					name: 'DCCA',
+					email: 'dcca12@gmail.com',
+					cart: {
+						items: [],
+					},
+				});
+				user.save();
+			}
+		});
+		app.listen(3000);
+		console.log('DB Connected!');
+	})
+	.catch((err) => {
+		console.log(err);
+	});
