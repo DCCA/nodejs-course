f65fdf9bafcd73d09ffabd1b7f1becf992ca26bc
dcca <dcca12@gmail.com>
2020-04-29 14:48:57 -0300

    add server-side input validation for the singup page

--- module11/controllers/auth.js
+++ module11/controllers/auth.js
@@ -4,6 +4,7 @@ const bcrypt = require('bcryptjs');
 const nodemailer = require('nodemailer');
 const sendgridTransport = require('nodemailer-sendgrid-transport');
 const SENDGRID_CREDENTIALS = require('../util/api-keys').SENDGRID_CREDENTIALS;
+const { validationResult } = require('express-validator/check');
 
 const User = require('../models/user');
 
@@ -78,21 +79,24 @@ exports.postLogin = (req, res, next) => {
 exports.postSignup = (req, res, next) => {
 	const email = req.body.email;
 	const password = req.body.password;
-	const confirmedPassword = req.body.confirmedPassword;
-	User.findOne({ email: email })
-		.then((userDoc) => {
-			if (userDoc) {
-				req.flash('error', 'E-mail already taken.');
-				return res.redirect('/signup');
-			}
-			return bcrypt.hash(password, 12).then((hashedPassword) => {
-				const user = new User({
-					email: email,
-					password: hashedPassword,
-					cart: { items: [] },
-				});
-				return user.save();
+	const errors = validationResult(req);
+	if (!errors.isEmpty()) {
+		// console.log(errors.array());
+		return res.status(422).render('auth/signup', {
+			path: '/signup',
+			pageTitle: 'Signup',
+			errorMessage: errors.array()[0].msg,
+		});
+	}
+	bcrypt
+		.hash(password, 12)
+		.then((hashedPassword) => {
+			const user = new User({
+				email: email,
+				password: hashedPassword,
+				cart: { items: [] },
 			});
+			return user.save();
 		})
 		.then((result) => {
 			res.redirect('/login');
--- module11/routes/auth.js
+++ module11/routes/auth.js
@@ -1,6 +1,8 @@
 const express = require('express');
+const { check, body } = require('express-validator/check');
 
 const authController = require('../controllers/auth');
+const User = require('../models/user');
 
 const router = express.Router();
 
@@ -10,7 +12,46 @@ router.get('/signup', authController.getSignup);
 
 router.post('/login', authController.postLogin);
 
-router.post('/signup', authController.postSignup);
+router.post(
+	'/signup',
+	// Validation block
+	[
+		// Check the specific value of that name. This is the name in the HTML.
+		check(
+			'email',
+			// Add a custom message
+			'Please enter a valid e-mail'
+		)
+			// Validate if is email
+			.isEmail()
+			// How to create a custom validation in express-validator
+			.custom((value, { req }) => {
+				// if (value === 'test@test.com') {
+				// 	throw new Error('This email address is forbidden');
+				// }
+				// return true;
+				return User.findOne({ email: value }).then((userDoc) => {
+					if (userDoc) {
+						return Promise.reject('E-mail already taken.');
+					}
+				});
+			}),
+		body(
+			'password',
+			// Message for all validations
+			'Please enter a password with only numbers and text and at least 5 characters'
+		)
+			.isLength({ min: 5 })
+			.isAlphanumeric(),
+		body('confirmPassword').custom((value, { req }) => {
+			if (value !== req.body.password) {
+				throw new Error("The passwords don't match ");
+			}
+			return true;
+		}),
+	],
+	authController.postSignup
+);
 
 router.post('/logout', authController.postLogout);
 
