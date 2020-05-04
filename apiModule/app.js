867fea8e6552344e8bb51a2607719cad8d80df6e
dcca <dcca12@gmail.com>
2020-05-04 15:18:43 -0300

    add update post route and controller

--- apiModule/app.js
+++ apiModule/app.js
@@ -42,7 +42,7 @@ app.use((req, res, next) => {
 	res.setHeader('Access-Control-Allow-Origin', '*');
 	// Set the methods
 	res.setHeader(
-		'Access-Control.Allow-Methods',
+		'Access-Control-Allow-Methods',
 		'GET, POST, PUT, PATCH, DELETE'
 	);
 	//
--- apiModule/routes/feed.js
+++ apiModule/routes/feed.js
@@ -9,12 +9,21 @@ router.get('/posts', feedController.getPosts);
 router.post(
 	'/posts',
 	[
-		body('title').trim().notEmpty().isLength({ min: 7 }),
+		body('title').trim().notEmpty().isLength({ min: 5 }),
 		body('content').trim().isLength({ min: 5 }),
 	],
 	feedController.createPost
 );
 
 router.get('/post/:postId', feedController.getPost);
+
+router.put(
+	'/post/:postId',
+	[
+		body('title').trim().notEmpty().isLength({ min: 5 }),
+		body('content').trim().isLength({ min: 5 }),
+	],
+	feedController.updatePost
+);
 
 module.exports = router;
